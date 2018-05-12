7c978936265a20346f5305005223353465767ac7
Haseeb Akbar <haseeb2000@hotmail.com>
2018-05-12 22:00:59 +0100

    Player able to move left and right.

---
+++ 2D Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D myRigidBody;
+    [SerializeField] float runSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Run();
+	}
+
+	private void Run()
+	{
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // either -1 or 1;
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+	}
+}
