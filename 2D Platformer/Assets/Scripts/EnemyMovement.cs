996898fb468b3c4d814c1c6a03b955bca422b296
Haseeb Akbar <haseeb2000@hotmail.com>
2018-05-13 20:11:33 +0100

    Enemy now collides with wall and changes direction with sprite.

--- 2D Platformer/Assets/Scripts/EnemyMovement.cs
+++ 2D Platformer/Assets/Scripts/EnemyMovement.cs
@@ -17,10 +17,17 @@ public class EnemyMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        MoveEnemy();
+        if (IsFacingRight()) myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+        else myRigidBody.velocity = new Vector2(-moveSpeed, 0f);
 	}
 
-    private void MoveEnemy(){
-        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
     }
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+        transform.localScale = new Vector2(-Mathf.Sign(myRigidBody.velocity.x), 1f);
+	}
 }
