5c3671995318aa9f9286d00c80b3b89a35dcf9f8
Haseeb Akbar <haseeb2000@hotmail.com>
2018-05-13 16:51:08 +0100

    Enemy animation created, also enemy can now move.

---
+++ 2D Platformer/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    //config
+    [SerializeField] float moveSpeed = 0.02f;
+
+    //cache component refrences
+    Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveEnemy();
+	}
+
+    private void MoveEnemy(){
+        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+}
