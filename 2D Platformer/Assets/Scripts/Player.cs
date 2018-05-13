23272d871973a2fc224f2960152cc8151d508835
Haseeb Akbar <haseeb2000@hotmail.com>
2018-05-13 12:08:12 +0100

    Sorted animation issues on the ladder. Also able to make gravity scale 0 when on ladder.

--- 2D Platformer/Assets/Scripts/Player.cs
+++ 2D Platformer/Assets/Scripts/Player.cs
@@ -18,6 +18,7 @@ public class Player : MonoBehaviour {
     Animator myAnimator;
     Rigidbody2D myRigidBody;
     Collider2D myCollider2D;
+    float gravityScaleAtStart;
 
 
 
@@ -26,14 +27,15 @@ public class Player : MonoBehaviour {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Run();
+        ClimbLadder();
         FlipSprite();
         Jump();
-        ClimbLadder();
 	}
 
 	private void Run()
@@ -41,20 +43,26 @@ public class Player : MonoBehaviour {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // either -1 or 1;
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
 	}
 
     private void ClimbLadder()
     {
-        
-        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) myAnimator.SetBool("Climbing", false);
 
-        else {
-            bool playerVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("Climbing", false);
+            return;
+        }
+            myRigidBody.gravityScale = 0f;
             float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
             Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
             myRigidBody.velocity = climbVelocity;
+            bool playerVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
             myAnimator.SetBool("Climbing", playerVerticalSpeed);
-        }
+
     }
 
 
@@ -64,13 +72,7 @@ public class Player : MonoBehaviour {
     private void FlipSprite(){
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon; // means greater than 0
 
-        if (playerHasHorizontalSpeed)
-        {
-            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
-            myAnimator.SetBool("Running", true);
-        }
-
-        else myAnimator.SetBool("Running", false);
+        if (playerHasHorizontalSpeed) transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
 
     }
 
